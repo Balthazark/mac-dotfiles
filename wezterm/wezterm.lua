1c3a6880e22ddc01690d73a8709f29c6eb13becb
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2024-10-09 22:46:53 +0200

    added wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ config = wezterm.config_builder()
 
 config = {
     automatically_reload_config = true,
-    enable_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
     window_close_confirmation = "NeverPrompt",
     window_decorations = "RESIZE",
     default_cursor_style = "BlinkingBar",
@@ -17,8 +17,43 @@ config = {
 			mods = 'CTRL',
 			action = wezterm.action.ToggleFullScreen,
 		},
-	
-	},
+  },
+  window_frame = {
+    inactive_titlebar_bg = '#11111b',
+    active_titlebar_bg = '#11111b',
+  },
+  colors = {
+  tab_bar = {
+      background = '#11111b',
+      active_tab = {
+          bg_color = '#1e1e2e',
+          fg_color = '#c0c0c0',
+          intensity = 'Normal',
+          underline = 'None',
+          italic = false,
+          strikethrough = false,
+      },
+      inactive_tab = {
+          bg_color = '#11111b',
+          fg_color = '#808080',
+      },
+      inactive_tab_hover = {
+          bg_color = '#45475a',
+          fg_color = '#909090',
+          italic = true,
+      },
+      new_tab = {
+          bg_color = '#1e1e2e',
+          fg_color = '#808080',
+      },
+      new_tab_hover = {
+          bg_color = '#45475a',
+          fg_color = '#909090',
+          italic = true,
+      },
+      inactive_tab_edge = "#11111b",
+  },
+},
 }
 
 return config
